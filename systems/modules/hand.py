4964a93115606c77df2e4e69c676859e7a7b8b32
WillDyer <will.dyer9@gmail.com>
2024-07-04 18:53:43 +0100

    better spacing

--- systems/modules/hand.py
+++ systems/modules/hand.py
@@ -1,5 +1,5 @@
 system = ["proximal","intermediate","distal","distalend"]
-system_pos = {"proximal": [2,0,0], "intermediate": [4,0,0], "distal": [6,0,0], "distalend": [8,0,0]}
+system_pos = {"proximal": [3,0,0], "intermediate": [6,0,0], "distal": [9,0,0], "distalend": [12,0,0]}
 system_rot = {"proximal": [0,0,0], "intermediate": [0,0,0], "distal": [0,0,0], "distalend": [0,0,0]}
 side = "_l"
 guide_scale = 0.25
