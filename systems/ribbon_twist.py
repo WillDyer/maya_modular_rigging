37c4db8cc293e74a0125c78b54d1241897f10f34
WillDyer <will.dyer9@gmail.com>
2024-07-23 13:13:46 +0100

    updates

--- systems/ribbon_twist.py
+++ systems/ribbon_twist.py
@@ -22,8 +22,38 @@ class prep_skeleton():
         self.joint_list = utils.get_joints_between(start_joint, end_joint)
         print(self.joint_list)
 
-    def insert_joints_between(self):
-        pass
+    def insert_joints_between(self, joint1, joint2, num_joints):
+        if num_joints <= 0:
+            print("Number of joints to insert must be greater than 0.")
+            return
+        tween_joint_list = []
+
+        # Get the positions of the existing joints
+        pos1 = cmds.xform(joint1, q=True, ws=True, t=True)
+        pos2 = cmds.xform(joint2, q=True, ws=True, t=True)
+
+        # Calculate the step increment for each joint
+        step = [(pos2[0] - pos1[0]) / (num_joints + 1),
+                (pos2[1] - pos1[1]) / (num_joints + 1),
+                (pos2[2] - pos1[2]) / (num_joints + 1)]
+
+        # Create and position the new joints
+        previous_joint = joint1
+        for i in range(1, num_joints + 1):
+            new_position = [pos1[0] + step[0] * i,
+                            pos1[1] + step[1] * i,
+                            pos1[2] + step[2] * i]
+            new_joint = cmds.joint(p=new_position,n="joint_#")
+            if i == 1:
+                first_joint = new_joint
+            tween_joint_list.append(new_joint)
+            previous_joint = new_joint
+
+        # Reparent the final joint to joint2
+        cmds.parent(joint2, previous_joint)
+        cmds.parent(first_joint, joint1)
+
+        cmds.joint(first_joint, edit=True, zso=1, oj="xyz", sao="xup", ch=True)
 
 
 class ribbon_twist():
