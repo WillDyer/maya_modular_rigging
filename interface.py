4f9ed6ae42f4896d85ac75dfe77a5d00f51f9508
WillDyer <will.dyer9@gmail.com>
2024-10-29 19:41:59 +0000

    init

--- interface.py
+++ interface.py
@@ -2,18 +2,28 @@ import maya.cmds as cmds
 from maya import OpenMayaUI as omui
 
 try:
-    from PySide6.QtCore import *
-    from PySide6.QtGui import *
-    from PySide6.QtWidgets import QWidget
-    from PySide6.QtWidgets import *
-    from PySide6.QtUiTools import *
+    from PySide6.QtCore import Qt
+    from PySide6.QtGui import QIcon
+    from PySide6.QtWidgets import (QWidget,
+                                   QVBoxLayout,
+                                   QHBoxLayout,
+                                   QPushButton,
+                                   QScrollArea,
+                                   QLabel,
+                                   QSizePolicy,
+                                   QLineEdit)
     from shiboken6 import wrapInstance
 except ModuleNotFoundError:
-    from PySide2.QtCore import *
-    from PySide2.QtGui import *
-    from PySide2.QtWidgets import QWidget
-    from PySide2.QtWidgets import *
-    from PySide2.QtUiTools import *
+    from PySide2.QtCore import Qt
+    from PySide2.QtGui import QIcon
+    from PySide2.QtWidgets import (QWidget,
+                                   QVBoxLayout,
+                                   QHBoxLayout,
+                                   QPushButton,
+                                   QScrollArea,
+                                   QLabel,
+                                   QSizePolicy,
+                                   QLineEdit)
     from shiboken2 import wrapInstance
 import importlib
 import os.path
--- user_interface/pages/module_settings.py
+++ user_interface/pages/module_settings.py
@@ -1,15 +1,25 @@
 try:
-    from PySide6.QtCore import *
-    from PySide6.QtGui import *
-    from PySide6.QtWidgets import QWidget
-    from PySide6.QtWidgets import *
-    from PySide6.QtUiTools import *
+    from PySide6.QtCore import Qt
+    from PySide6.QtGui import QIcon
+    from PySide6.QtWidgets import (QWidget,
+                                   QVBoxLayout,
+                                   QHBoxLayout,
+                                   QPushButton,
+                                   QScrollArea,
+                                   QLabel,
+                                   QSizePolicy,
+                                   QLineEdit)
 except ModuleNotFoundError:
-    from PySide2.QtCore import *
-    from PySide2.QtGui import *
-    from PySide2.QtWidgets import QWidget
-    from PySide2.QtWidgets import *
-    from PySide2.QtUiTools import *
+    from PySide2.QtCore import Qt
+    from PySide2.QtGui import QIcon
+    from PySide2.QtWidgets import (QWidget,
+                                   QVBoxLayout,
+                                   QHBoxLayout,
+                                   QPushButton,
+                                   QScrollArea,
+                                   QLabel,
+                                   QSizePolicy,
+                                   QLineEdit)
 
 import maya.cmds as cmds
 import sys
