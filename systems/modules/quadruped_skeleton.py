2ee71dc6c04c5d486728712b73243f4a83ffb59c
WillDyer <will.dyer9@gmail.com>
2024-10-31 14:41:28 +0000

    added tail

--- systems/modules/quadruped_skeleton.py
+++ systems/modules/quadruped_skeleton.py
@@ -2,7 +2,8 @@ is_preset = True
 module_to_be_made = {"basic_root": "world",
                      "quadruped_spine": "COG",
                      "quadruped_leg": "COG",
-                     "quadruped_arm": "spine_4"}
+                     "quadruped_arm": "spine_4",
+                     "tail": "spine_1"}
 
 guide_scale = 1
 space_swapping = []
