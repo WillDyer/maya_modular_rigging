252905ec7314ffc6323651d249802d8ff30826a1
WillDyer <will.dyer9@gmail.com>
2024-10-31 14:29:17 +0000

    updates

---
+++ systems/modules/quadruped_skeleton.py
@@ -0,0 +1,8 @@
+is_preset = True
+module_to_be_made = {"basic_root": "world",
+                     "quadruped_spine": "COG",
+                     "quadruped_leg": "COG",
+                     "quadruped_arm": "spine_4"}
+
+guide_scale = 1
+space_swapping = []
