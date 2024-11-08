96f97dacc68b15efd5e24c4d17dc5b654f21c32c
WillDyer <will.dyer9@gmail.com>
2024-11-08 19:06:07 +0000

    fixed connection

--- systems/modules/boar_skeleton.py
+++ systems/modules/boar_skeleton.py
@@ -2,7 +2,7 @@ is_preset = True
 hide = False
 module_to_be_made = {"boar_root": "world",
                      "boar_spine": "COG",
-                     "boar_leg": "COG",
+                     "boar_leg": "spine_1",
                      "boar_arm": "spine_4",
                      "boar_tail": "spine_1"}
 
