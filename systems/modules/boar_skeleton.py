0ae458ed46403959f5285c88e7767e3222ce64bf
WillDyer <will.dyer9@gmail.com>
2024-11-08 22:22:16 +0000

    added head module

--- systems/modules/boar_skeleton.py
+++ systems/modules/boar_skeleton.py
@@ -4,7 +4,8 @@ module_to_be_made = {"boar_root": "world",
                      "boar_spine": "COG",
                      "boar_leg": "spine_1",
                      "boar_arm": "spine_4",
-                     "boar_tail": "spine_1"}
+                     "boar_tail": "spine_1",
+                     "boar_head": "kneck_3"}
 
 guide_scale = 1
 space_swapping = []
