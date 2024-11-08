c20e9725b0ad7641366c177cc3a02f09fc2536ae
WillDyer <will.dyer9@gmail.com>
2024-11-08 18:10:14 +0000

    init

---
+++ systems/modules/boar_skeleton.py
@@ -0,0 +1,10 @@
+is_preset = True
+hide = False
+module_to_be_made = {"basic_root": "world",
+                     "boar_spine": "COG",
+                     "boar_leg": "COG",
+                     "boar_arm": "spine_4",
+                     "boar_tail": "spine_1"}
+
+guide_scale = 1
+space_swapping = []
