12603ca135fc6053b8f124ab8fef1de54aa1fa87
WillDyer <will.dyer9@gmail.com>
2024-11-08 22:22:05 +0000

    init

---
+++ systems/modules/boar_head.py
@@ -0,0 +1,13 @@
+is_preset = False
+hide = True
+ignore_transforms = True
+system = ['head', 'jaw_1', 'jaw_2', 'jaw_3']
+system_pos = {'head': [0.0, 93.18228724643987, 79.61659463739836], 'jaw_1': [0.0, 88.67387486883482, 82.10254164934881], 'jaw_2': [0.0, 77.46604596749889, 82.10254164934881], 'jaw_3': [0.0, 66.46889054175197, 105.52943213484797]}
+system_rot = {'head': [0.0, 0.0, 0.0], 'jaw_1': [0.0, 0.0, 0.0], 'jaw_2': [0.0, 0.0, 0.0], 'jaw_3': [0.0, 0.0, 0.0]}
+side = "C"
+space_swapping = []
+guide_scale = 1
+available_rig_types = ["FK"]
+default_ctrl_shape = {
+    "fk_head": "cube"
+}
