e246698309264a4a9045e1f73d492e1b60ece865
WillDyer <will.dyer9@gmail.com>
2024-12-09 16:43:49 +0000

    moved main to init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from importlib import reload
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+print(f"Adding to MAYA_SCRIPT_PATH ENV {os.path.dirname(os.path.abspath(__file__))}")
+
+import interface
+reload(interface)
+
+interface.main()
+
