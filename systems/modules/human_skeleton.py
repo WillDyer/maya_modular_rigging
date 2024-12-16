1cb456b494af7e0239af79e822868aa842cdde28
WillDyer <will.dyer9@gmail.com>
2024-12-16 17:52:01 +0000

    rm

--- systems/modules/human_skeleton.py
+++
@@ -1,9 +0,0 @@
-is_preset = True
-hide = False
-module_to_be_made = {"basic_root": "world",
-                     "biped_spine": "COG",
-                     "biped_arm": "spine_4",
-                     "biped_leg": "COG"}
-
-guide_scale = 1
-space_swapping = []
