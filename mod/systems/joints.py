c058d4691f0f4285ff8086615ec46c2be4e38f45
WillDyer <will.dyer9@gmail.com>
2024-12-30 17:05:17 +0000

    fixed bad orientation

--- mod/systems/joints.py
+++ mod/systems/joints.py
@@ -27,10 +27,6 @@ def joint(orientation,top_skeleton_joint, system):
         list.append(top_skeleton_joint)
     list.reverse()
 
-    """side = cmds.getAttr(f"{top_skeleton_joint}.module_side",asString=1)
-    if side == "None":
-        side = """""
-
     for x in list:
         if "cluster" in x or "data_" in x:
             pass
@@ -39,21 +35,22 @@ def joint(orientation,top_skeleton_joint, system):
 
     cmds.select(clear=1)
     for locator in list_ctrls:
-        loc = cmds.xform(locator, r=True, ws=True, q=True, t=True)  # Gather locator location
-        rot = cmds.xform(locator, r=True, ws=True, q=True, ro=True)
+        loc = cmds.xform(locator, q=True, ws=True, t=True)  # Gather worldspace location
+        rot = cmds.xform(locator, q=True, ws=True, ro=True) # gather worldspace rotation
         jnt_name = cmds.joint(n=f"{joint_tag}{locator}", p=loc)  # create joint based off the location
-        cmds.xform(jnt_name, r=True, ro=rot)
-        cmds.makeIdentity(jnt_name, apply=True, t=False, r=True, s=False)
+        cmds.xform(jnt_name, ws=True, ro=rot)
+        # cmds.makeIdentity(jnt_name, apply=True, t=False, r=True, s=False)
         jnt_names.append(jnt_name)
 
     mirror_attribute = cmds.getAttr(f"{top_skeleton_joint}.mirror_orientation", asString=1)
     if mirror_attribute == "Yes": sao_axis = f"{orientation[0]}down"
     else: sao_axis = f"{orientation[0]}up"
-
+    
+    ### Old orientation code - keeping in case needed ###
     # Orient joint
-    cmds.joint(f"{joint_tag}{list_ctrls[0]}", edit=True, zso=1, oj=orientation, sao=sao_axis, ch=True)
+    # cmds.joint(f"{joint_tag}{list_ctrls[0]}", edit=True, zso=1, oj=orientation, sao=sao_axis, ch=True)
     # Orient end joint to world
-    cmds.joint(f"{joint_tag}{list_ctrls[-1]}", e=True, oj="none",ch=True, zso=True)
+    # cmds.joint(f"{joint_tag}{list_ctrls[-1]}", e=True, oj="none",ch=True, zso=True)
 
     cmds.setAttr(f"{joint_tag}{list_ctrls[0]}.overrideEnabled",1)
     return jnt_names
