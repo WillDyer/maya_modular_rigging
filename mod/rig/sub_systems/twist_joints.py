c3a402514c6a3d89a3b47ed27c452bc814638b3e
WillDyer <will.dyer9@gmail.com>
2025-01-25 19:06:05 +0000

    feat: changed how twist works, better orientation support

--- mod/rig/sub_systems/twist_joints.py
+++ mod/rig/sub_systems/twist_joints.py
@@ -156,6 +156,7 @@ class CreateTwist():
     def __init__(self, twist_joint_dict, system, key):
         self.system = system
         self.key = key
+
         # unpack dictionary
         self.joint1 = twist_joint_dict["joint1"]
         self.joint1_twist = twist_joint_dict["joint1_twist"]
@@ -164,65 +165,129 @@ class CreateTwist():
         self.tween_joints = twist_joint_dict["tween_joints"]
         self.tweak_joints = twist_joint_dict["tweak_joints"]
 
+        self.module = importlib.import_module(f"mod.modules.{self.key['module']}")
+        importlib.reload(self.module)
+        
+        self.offset = -10
+        self.offset_locator = -10
+        self.aim_axis = -1
+        try:
+            if cmds.getAttr(f"{self.key['master_guide']}.is_mirrored", asString=True) == "Yes":
+                self.offset_locator = 10
+                self.aim_axis = 1
+        except KeyError:
+            pass
+
         if system == "rig":
-            self.ik_handle()
-            self.correct_orientation()
-            self.twist_constraints()
-            self.parent_to_heirachy()
+            if self.module.twist_joint["end"] in self.joint2: # treat end twist differently
+                cmds.joint(self.joint2_twist, e=True, oj="none",ch=True, zso=True)
+                self.correct_orientation(list=self.tween_joints, target=self.joint2)
+                self.reverse_twist()
+                self.tween_falloff(source_rot=self.joint2_twist)
+                self.parent_to_heirachy(parent_type="specific", joint=self.joint1)
+            else:
+                cmds.joint(self.joint2_twist, e=True, oj="none",ch=True, zso=True)
+                root_orient, end_orient, aim_loc = self.create_orientation()
+                self.stable_orientation(top_twist=root_orient, end_twist=end_orient, loc=aim_loc)
+                self.tween_falloff(source_rot=self.joint1)
+                self.parent_to_heirachy(parent_type="parent")
+                cmds.group(root_orient, n=f"grp_twist_jnts_{self.key['master_guide']}", w=1)
         elif system == "skn":
             self.parent_to_heirachy()
         else:
-            cmds.warning("Twist joint cannot be set on this system.")
+            cmds.warning("twist_joints: Twist joint cannot be set on this system.")
 
-    def ik_handle(self):
+    def ik_handle(self, start_joint="", end_joint=""):
         cmds.select(clear=True)
-        self.ik_hdl = cmds.ikHandle(n=f"hdl_{self.joint1_twist}", sj=self.joint1_twist, ee=self.joint2_twist, solver="ikSCsolver")[0]
-        cmds.pointConstraint(self.joint2, f"hdl_{self.joint1_twist}")
+        ik_hdl = cmds.ikHandle(n=f"hdl_{start_joint}", sj=start_joint, ee=end_joint, solver="ikRPsolver")[0]
+        return ik_hdl
 
-    def correct_orientation(self):
-        orient_constraint = cmds.orientConstraint(self.joint1, self.joint2_twist)
-        cmds.delete(orient_constraint)
-        multiply_divide_node = cmds.shadingNode("multiplyDivide",au=1, n=f"{self.joint2_twist}_orient")
-        cmds.connectAttr(f"{self.joint1}.rotateX", f"{multiply_divide_node}.input1X", f=True)
-        cmds.connectAttr(f"{multiply_divide_node}.outputX", f"{self.joint2_twist}.rotateX", f=True)
-        for input in ["input2X", "input2Y", "input2Z"]:
-            cmds.setAttr(f"{multiply_divide_node}.{input}", 0.5)
+    def correct_orientation(self, list=[], target=""):
+        for item in list:
+            aim_const = cmds.aimConstraint(target, item, u=[1,0,0])
+            cmds.delete(aim_const)
+            cmds.makeIdentity(item, apply=True, t=False, r=True, s=False)
 
-    def twist_constraints(self):
-        if len(self.tween_joints) > 3:
-            cmds.error("self.tween_joints var cannot be greater than 3")
+    def create_orientation(self):
+        duplicate_list = []
+        orient_suffix = "orient"
+
+        self.correct_orientation(list=self.tween_joints, target=self.joint2)
+        
+        root_orient = cmds.duplicate(self.joint1_twist, n=f"{self.joint1_twist}_{orient_suffix}", po=True)[0]
+        for joint in self.tween_joints:
+            dupe = cmds.duplicate(joint, n=f"{joint}_{orient_suffix}", po=True)[0]
+            cmds.parent(dupe, w=True)
+            duplicate_list.append(dupe)
+        
+        cmds.parent(duplicate_list, root_orient)
+        
+        cmds.xform(root_orient, translation=(0,0,self.offset), r=True)
+        aim_loc = cmds.spaceLocator(n=f"{self.joint1_twist.replace('jnt_rig','loc')}_aim")[0]
+        cmds.matchTransform(aim_loc, root_orient)
+        cmds.parent(aim_loc, root_orient)
+        cmds.xform(aim_loc, translation=(0,0,self.offset_locator), r=True)
+        
+        cmds.hide(root_orient)
+        cmds.select(clear=True)
+        return root_orient, duplicate_list[-1], aim_loc
+
+    def stable_orientation(self, top_twist="", end_twist="", loc=""):
+        cmds.aimConstraint(self.joint2, self.joint1_twist, u=[0,0,self.aim_axis], wut="object", wuo=loc, mo=True)
+
+        handle = self.ik_handle(start_joint=top_twist, end_joint=end_twist)
+        cmds.parent(handle, self.joint2)
+        cmds.matchTransform(handle, self.joint2)
+        cmds.hide(handle)
+
+        for XYZ in ["X","Y","Z"]:
+            cmds.setAttr(f"{handle}.poleVector{XYZ}",0)
+
+        connect_to = self.key["system_to_connect"]
+        if connect_to:
+            cmds.parentConstraint(f"jnt_rig_{connect_to[1]}", top_twist, mo=True, n=f"pConst_{top_twist.replace('jnt_rig_','')}")
         else:
-            for joint in self.tween_joints:
-                cmds.orientConstraint([self.joint1_twist, self.joint2_twist], joint, n=f"orientConst_{joint}")
-                cmds.pointConstraint([self.joint1_twist, self.joint2_twist], joint, n=f"pointConst_{joint}")
+            cmds.parentConstraint(self.key["joints"][0], top_twist, mo=True, n=f"pConst_{top_twist.replace('jnt_rig_','')}")
 
-            if len(self.tween_joints) == 1:
+    def tween_falloff(self, source_rot=""):
+        def get_middle_values(length=0):
+            if length <= 0:
+                return []
+            return [round(i / (length + 1), 2) for i in range(1, length + 1)]
+            
+        multi_node = cmds.createNode('multiplyDivide', name=f'multi_{source_rot}_twist')
+        
+        XYZ = ["X","Y","Z"]
+        if len(self.tween_joints) <= 3:
+            values = get_middle_values(length=len(self.tween_joints))
+        else:
+            values = []
+
+        for i, joint in enumerate(self.tween_joints):
+            try:
+                cmds.connectAttr(f"{source_rot}.rotateX", f"{multi_node}.input1{XYZ[i]}")
+                cmds.connectAttr(f"{multi_node}.output{XYZ[i]}", f"{joint}.rotateX")
+                cmds.setAttr(f"{multi_node}.input2{XYZ[i]}", values[i])
+            except Exception:
                 pass
-            elif len(self.tween_joints) == 2:
-                cmds.setAttr(f"orientConst_{self.tween_joints[0]}.{self.joint1_twist}W0",2)
-                cmds.setAttr(f"pointConst_{self.tween_joints[0]}.{self.joint1_twist}W0",2)
 
-                cmds.setAttr(f"orientConst_{self.tween_joints[1]}.{self.joint2_twist}W1",2)
-                cmds.setAttr(f"pointConst_{self.tween_joints[1]}.{self.joint2_twist}W1",2)
-            elif len(self.tween_joints) == 3:
-                cmds.setAttr(f"orientConst_{self.tween_joints[0]}.{self.joint1_twist}W0",3)
-                cmds.setAttr(f"pointConst_{self.tween_joints[0]}.{self.joint1_twist}W0",3)
+    def reverse_twist(self):
+        aim_loc = cmds.spaceLocator(n=f"{self.joint2_twist.replace('jnt_rig_','')}_aim")[0]
+        cmds.matchTransform(aim_loc, self.joint2_twist)
+        cmds.parent(aim_loc, self.joint2)
+        cmds.xform(aim_loc, translation=(0,0,self.offset_locator), r=True)
 
-                cmds.setAttr(f"orientConst_{self.tween_joints[2]}.{self.joint2_twist}W1",3)
-                cmds.setAttr(f"pointConst_{self.tween_joints[2]}.{self.joint2_twist}W1",3)
+        cmds.aimConstraint(self.joint1, self.joint2_twist, u=[0,0,self.aim_axis], wut="object", wuo=aim_loc, mo=True)
+        cmds.hide(aim_loc)
 
-        # checks if twist enabled if yes. point constraint to follow stretch
-        if cmds.getAttr(f"{self.key['master_guide']}.{self.key['master_guide']}_squash_stretch", asString=1) == "Yes":
-            cmds.pointConstraint(self.joint1, self.joint1_twist, n=f"pointConst_{self.joint1_twist}")
-            cmds.pointConstraint(self.joint2, self.joint2_twist, n=f"pointConst_{self.joint2_twist}")
-
-    def parent_to_heirachy(self):
-        parent_joint = cmds.listRelatives(self.joint1, parent=True)
-        if parent_joint is None:
-            parent_joint = self.joint1
-        cmds.parent(self.joint1_twist, parent_joint)
-        if self.system == "rig":
-            cmds.parent(self.ik_hdl, parent_joint)
+    def parent_to_heirachy(self, parent_type="", joint=""):
+        if parent_type == "parent":
+            parent_joint = cmds.listRelatives(self.joint1, parent=True)
+            if parent_joint is None:
+                parent_joint = self.joint1
+            cmds.parent(self.joint1_twist, parent_joint)
+        elif parent_type == "specific":
+            cmds.parent(self.joint1_twist, joint)
 
 
 class CreateTweaks():
