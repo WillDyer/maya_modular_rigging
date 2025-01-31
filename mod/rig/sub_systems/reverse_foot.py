22c961e00706156ef43a1dbe7cf55d9b128e4703
Dyer <wd22aaa@herts.ac.uk>
2025-01-31 15:54:40 +0000

    refactor: changed attr that parents to module

--- mod/rig/sub_systems/reverse_foot.py
+++ mod/rig/sub_systems/reverse_foot.py
@@ -248,7 +248,8 @@ class CreateReverseFootQuadruped():
         jnt_verification_values = [self.module.rev_locators["ankle"],self.module.rev_locators["ball"],self.module.rev_locators["toe"]]
         self.jnt_list = [item for item in self.system["ik_joint_list"] if any(key in item for key in jnt_verification_values)]
 
-        self.foot_ctrl = [x for x in self.system["ik_ctrl_list"] if cmds.attributeQuery("handle", node=x, exists=True) and cmds.getAttr(f"{x}.handle", asString=1) == "True"][0]
+        self.foot_ctrl = [x for x in self.system["ik_ctrl_list"] if cmds.attributeQuery("reverse_parent", node=x, exists=True) and cmds.getAttr(f"{x}.reverse_parent", asString=1) == "True"][0]
+        print(f"REVERSE FOOT: {self.foot_ctrl}")
 
         cmds.parent(self.rev_list[0],self.reverse_foot_data["loc_ankle"])
         
