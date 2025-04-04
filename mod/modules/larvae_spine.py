1f5284d7a038121bf4a304dbcdb38c7f306702e7
WillDyer <will.dyer9@gmail.com>
2025-04-04 01:48:59 +0100

    refactor: small adjustments for rig

--- mod/modules/larvae_spine.py
+++ mod/modules/larvae_spine.py
@@ -1,12 +1,12 @@
 is_preset = False
 hide = False
 ignore_transforms = False
-system = ['spine_1', 'spine_2', 'spine_3', 'spine_4', 'spine_5', 'spine_6', 'spine_7', 'spine_8', 'spine_9', 'spine_10']
-system_pos = {'spine_1': [0.0, 0.0, 0.0], 'spine_2': [25.0, 0.0, 0.0], 'spine_3': [50.0, 0.0, 0.0], 'spine_4': [75.0, 0.0, 0.0], 'spine_5': [100.0, 0.0, 0.0], 'spine_6': [125.0, 0.0, 0.0], 'spine_7': [150.0, 0.0, 0.0], 'spine_8': [175.0, 0.0, 0.0], 'spine_9': [200.0, 0.0, 0.0], 'spine_10': [225.0, 0.0, 0.0]}
-system_rot = {'spine_1': [0.0, 0.0, 0.0], 'spine_2': [0.0, 0.0, 0.0], 'spine_3': [0.0, 0.0, 0.0], 'spine_4': [0.0, 0.0, 0.0], 'spine_5': [0.0, 0.0, 0.0], 'spine_6': [0.0, 0.0, 0.0], 'spine_7': [0.0, 0.0, 0.0], 'spine_8': [0.0, 0.0, 0.0], 'spine_9': [0.0, 0.0, 0.0], 'spine_10': [0.0, 0.0, 0.0]}
+system = ['spine_1', 'spine_2', 'spine_3', 'spine_4', 'spine_5', 'spine_6', 'spine_7', 'spine_8']
+system_pos = {'spine_1': [0.0, 0.0, 0.0], 'spine_2': [25.0, 0.0, 0.0], 'spine_3': [50.0, 0.0, 0.0], 'spine_4': [75.0, 0.0, 0.0], 'spine_5': [100.0, 0.0, 0.0], 'spine_6': [125.0, 0.0, 0.0], 'spine_7': [150.0, 0.0, 0.0], 'spine_8': [175.0, 0.0, 0.0]}
+system_rot = {'spine_1': [0.0, 0.0, 0.0], 'spine_2': [0.0, 0.0, 0.0], 'spine_3': [0.0, 0.0, 0.0], 'spine_4': [0.0, 0.0, 0.0], 'spine_5': [0.0, 0.0, 0.0], 'spine_6': [0.0, 0.0, 0.0], 'spine_7': [0.0, 0.0, 0.0], 'spine_8': [0.0, 0.0, 0.0]}
 ik_joints = {
     "start_joint": "spine_1",
-    "end_joint": "spine_10",
+    "end_joint": "spine_8",
     "pv_joint": None,
     "world_orientation": True,
     "offset_ctrl": False
@@ -16,4 +16,9 @@ space_swapping = []
 guide_scale = 1
 available_rig_types = ["FK","IK_Ribbon","Hybrid_FKIK"]
 locked_guide_attr = ["translateY"]
+default_ctrl_shape = {
+    "ik_spine_1": "cube",
+    "ik_spine_4": "cube",
+    "ik_spine_8": "cube"
+}
 
