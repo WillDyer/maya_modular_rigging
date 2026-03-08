f7a66819a3631e3e1160434907308979fa2125cb
WillDyer <will.dyer9@gmail.com>
2026-03-08 15:51:54 +0000

    feat: new null object module

---
+++ mod/modules/null_joint.py
@@ -0,0 +1,14 @@
+is_preset = False
+hide = False
+ignore_transforms = False
+delete_end = False
+system = ['null']
+system_pos = {'null': [0.0, 0.0, 0.0]}
+system_rot = {'null': [0.0, 0.0, 0.0]}
+side = "L"
+space_swapping = []
+guide_scale = 1
+available_rig_types = ["FK"]
+default_ctrl_shape = {
+    "fk_null": "cube"
+}
