55968af6530c550856d131b14a71b269c35ffd3a
WillDyer <will.dyer9@gmail.com>
2026-03-15 11:50:30 +0000

    fix: fixed issue with control shapes not being made on rig process

--- mod/modules/head.py
+++ mod/modules/head.py
@@ -5,16 +5,10 @@ delete_end = True
 system = ['head', 'jaw_1', 'jaw_2', 'jaw_3']
 system_pos = {'head': [0.0, 93.18228724643987, 79.61659463739836], 'jaw_1': [0.0, 88.67387486883482, 82.10254164934881], 'jaw_2': [0.0, 77.46604596749889, 82.10254164934881], 'jaw_3': [1.9721522630525295e-30, 66.46889054175196, 105.52943213484798]}
 system_rot = {'head': [0.0, 0.0, 0.0], 'jaw_1': [0.0, 0.0, -89.99999999999999], 'jaw_2': [7.48481400317823e-15, -64.85349103249379, -90.0], 'jaw_3': [7.48481400317823e-15, -64.85349103249379, -90.0]}
-ik_joints = {
-    "start_joint": "spine_1",
-    "end_joint": "spine_10",
-    "pv_joint": None,
-    "world_orientation": True
-}
 side = "C"
 space_swapping = []
 guide_scale = 1
-available_rig_types = ["FK","IK_Ribbon"]
+available_rig_types = ["FK"]
 default_ctrl_shape = {
     "fk_head": "cube"
 }
